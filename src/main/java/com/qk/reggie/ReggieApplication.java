7e5558e63a10849b14292e9ec1660456d25d119d
35890 <35890226@qq.com>
2022-05-27 17:34:06 +0800

    初始项目的创建

---
+++ src/main/java/com/qk/reggie/ReggieApplication.java
@@ -0,0 +1,15 @@
+package com.qk.reggie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@Slf4j    //加载日志
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class,args);
+        log.info("项目启动成功！");
+    }
+}
