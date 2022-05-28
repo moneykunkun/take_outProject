cdbf9edb35e3d4fa6ef19f989256a14cca3dfc95
35890 <35890226@qq.com>
2022-05-29 00:07:39 +0800

    新增登录拦截器，用户未登录无法跳转到后台页面

--- src/main/java/com/qk/reggie/ReggieApplication.java
+++ src/main/java/com/qk/reggie/ReggieApplication.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @Slf4j    //加载日志
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@ServletComponentScan               //扫描过滤器
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class,args);
---
+++ src/main/java/com/qk/reggie/filter/LoginCheckFilter.java
@@ -0,0 +1,25 @@
+package com.qk.reggie.filter;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 检查用户是否登录
+ */
+@Slf4j
+@WebFilter(filterName = "loginCheckFilter",urlPatterns = "/*")          //拦截路径
+public class LoginCheckFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request =(HttpServletRequest)servletRequest;
+        HttpServletResponse response= (HttpServletResponse)servletResponse;
+        log.info("拦截到请求 ：{}",request.getRequestURI());
+        //放行
+        filterChain.doFilter(request,response);
+    }
+}
