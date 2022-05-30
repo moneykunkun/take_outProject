421c3e3eac42c912d9b3519b83b0c2a88adadd67
35890 <35890226@qq.com>
2022-05-30 23:43:55 +0800

    新增Category菜品类 新增菜品分页查询

--- src/main/java/com/qk/reggie/controller/CategoryController.java
+++ src/main/java/com/qk/reggie/controller/CategoryController.java
@@ -1,14 +1,13 @@
 package com.qk.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.qk.reggie.common.R;
 import com.qk.reggie.entity.Category;
 import com.qk.reggie.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 分类管理
@@ -31,5 +30,25 @@ public class CategoryController {
         log.info("category：{}",category);
         categoryService.save(category);
         return  R.success("新增分类成功！");
+    }
+
+    /**
+     * 分页查询
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize){
+        //分页构造器
+        Page<Category> pageInfo =new Page<>(page,pageSize);
+        //条件构造器
+        LambdaQueryWrapper<Category> queryWrapper =new LambdaQueryWrapper<>();
+        //添加排序条件，根据sort字段排序
+        queryWrapper.orderByAsc(Category::getSort);
+
+        //分页查询
+        categoryService.page(pageInfo,queryWrapper);
+        return R.success(pageInfo);
     }
 }
