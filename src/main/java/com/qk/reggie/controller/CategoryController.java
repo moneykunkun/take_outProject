75d0eab50e08410eca1902b8aa22df40466769b9
35890 <35890226@qq.com>
2022-05-30 23:34:34 +0800

    新增Category菜品类 新增菜品分类功能

--- src/main/java/com/qk/reggie/controller/CategoryController.java
+++ src/main/java/com/qk/reggie/controller/CategoryController.java
@@ -1,8 +1,12 @@
 package com.qk.reggie.controller;
 
+import com.qk.reggie.common.R;
+import com.qk.reggie.entity.Category;
 import com.qk.reggie.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,4 +20,16 @@ public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
+
+    /**
+     * 新增分类
+     * @param category
+     * @return
+     */
+    @PostMapping
+    public R<String> save(@RequestBody Category category){
+        log.info("category：{}",category);
+        categoryService.save(category);
+        return  R.success("新增分类成功！");
+    }
 }
