c0a52f38cf77abdad56e0a842986728036ff4c4d
35890 <35890226@qq.com>
2022-06-01 16:18:25 +0800

    新增菜品口味的实体类及其接口业务代码

---
+++ src/main/java/com/qk/reggie/service/DishFlavorService.java
@@ -0,0 +1,9 @@
+package com.qk.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.qk.reggie.entity.DishFlavor;
+import org.springframework.stereotype.Service;
+
+
+public interface DishFlavorService extends IService<DishFlavor> {
+}
