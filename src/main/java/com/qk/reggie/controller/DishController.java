d53ae47609abf0727911a752d1e17b3cf23234df
35890 <35890226@qq.com>
2022-06-01 18:13:20 +0800

    新增菜品口味的管理controller

---
+++ src/main/java/com/qk/reggie/controller/DishController.java
@@ -0,0 +1,19 @@
+package com.qk.reggie.controller;
+
+import com.qk.reggie.service.DishFlavorService;
+import com.qk.reggie.service.DishService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 菜品管理
+ */
+@RestController
+@RequestMapping("/dish")
+public class DishController {
+    @Autowired
+    private DishService dishService;
+    @Autowired
+    private DishFlavorService dishFlavorService;
+}
