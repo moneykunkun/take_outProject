c5cb39f7837c8ea3ba12f28dde214b36c6fed774
35890 <35890226@qq.com>
2022-06-02 16:20:24 +0800

    更新

--- src/main/java/com/qk/reggie/service/impl/DishFlavorServiceImpl.java
+++ src/main/java/com/qk/reggie/service/impl/DishFlavorServiceImpl.java
@@ -3,7 +3,6 @@ package com.qk.reggie.service.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.qk.reggie.entity.DishFlavor;
 import com.qk.reggie.mapper.DishFlavorMapper;
-import com.qk.reggie.mapper.DishMapper;
 import com.qk.reggie.service.DishFlavorService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
