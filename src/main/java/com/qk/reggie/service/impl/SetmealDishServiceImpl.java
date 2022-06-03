72770b1da9c9e7c2b3bc76c70d0ed4a788f543b4
35890 <35890226@qq.com>
2022-06-03 12:41:51 +0800

    新增菜品实体及其接口

---
+++ src/main/java/com/qk/reggie/service/impl/SetmealDishServiceImpl.java
@@ -0,0 +1,13 @@
+package com.qk.reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.qk.reggie.entity.SetmealDish;
+import com.qk.reggie.mapper.SetmealDishMapper;
+import com.qk.reggie.service.SetmealDishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class SetmealDishServiceImpl extends ServiceImpl<SetmealDishMapper, SetmealDish> implements SetmealDishService {
+}
