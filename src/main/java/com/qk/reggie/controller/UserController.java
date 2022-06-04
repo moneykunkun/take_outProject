b76fdca96e5bb03a2b320c9c58f7b337690ae2ab
35890 <35890226@qq.com>
2022-06-04 09:41:48 +0800

    移动端 新增用户实体类的controller

---
+++ src/main/java/com/qk/reggie/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.qk.reggie.controller;
+
+import com.qk.reggie.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+}
