f820af203f11ba2c91e95d8dd9ebd3f0f757e43b
35890 <35890226@qq.com>
2022-06-06 14:40:53 +0800

    移动端 新增购物车实体类及其业务接口

---
+++ src/main/java/com/qk/reggie/mapper/ShoppingCartMappper.java
@@ -0,0 +1,9 @@
+package com.qk.reggie.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.qk.reggie.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ShoppingCartMappper extends BaseMapper<ShoppingCart> {
+}
