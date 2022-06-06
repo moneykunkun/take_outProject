3f847d8409dc7e1f810af0fcabaaefa1c4927c6c
35890 <35890226@qq.com>
2022-06-06 16:14:54 +0800

    移动端 新增订单和订单明细的controller

---
+++ src/main/java/com/qk/reggie/controller/OrderController.java
@@ -0,0 +1,17 @@
+package com.qk.reggie.controller;
+
+import com.qk.reggie.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+}
