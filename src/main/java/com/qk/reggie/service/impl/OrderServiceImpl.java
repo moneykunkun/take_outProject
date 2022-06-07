a49fd6dda27c35db611239f20813c6177850b021
35890 <35890226@qq.com>
2022-06-07 18:32:13 +0800

    移动端 完善下单逻辑

--- src/main/java/com/qk/reggie/service/impl/OrderServiceImpl.java
+++ src/main/java/com/qk/reggie/service/impl/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package com.qk.reggie.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.IdWorker;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.qk.reggie.common.BaseContext;
 import com.qk.reggie.common.CustomException;
@@ -9,15 +10,14 @@ import com.qk.reggie.entity.Orders;
 import com.qk.reggie.entity.ShoppingCart;
 import com.qk.reggie.entity.User;
 import com.qk.reggie.mapper.OrderMapper;
-import com.qk.reggie.service.AddressBookService;
-import com.qk.reggie.service.OrderService;
-import com.qk.reggie.service.ShoppingCartService;
-import com.qk.reggie.service.UserService;
+import com.qk.reggie.service.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -32,6 +32,9 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Orders> implement
 
     @Autowired
     private AddressBookService addressBookService;
+
+    @Autowired
+    private OrderDetailService orderDetailService;          //订单明细
     /**
      * 用户下单
      * @param orders
@@ -62,6 +65,23 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Orders> implement
              throw new CustomException("地址信息为空，不能下单");
          }
         //向订单表中添加数据
+        final long orderId = IdWorker.getId();
+        //设置订单实体的其他属性
+        orders.setId(orderId);
+        orders.setOrderTime(LocalDateTime.now());
+        orders.setCheckoutTime(LocalDateTime.now());
+        orders.setStatus(2);        //待派送
+        orders.setAmount(new BigDecimal(amount.get()));//总金额
+        orders.setUserId(currentId);        //用户id
+        orders.setNumber(String.valueOf(orderId));      //订单号
+        orders.setUserName(user.getName());
+        orders.setConsignee(addressBook.getConsignee());            //收货人
+        orders.setPhone(addressBook.getPhone());            //手机号
+        orders.setAddress((addressBook.getProvinceName() == null ? "" : addressBook.getProvinceName())
+                + (addressBook.getCityName() == null ? "" : addressBook.getCityName())
+                + (addressBook.getDistrictName() == null ? "" : addressBook.getDistrictName())
+                + (addressBook.getDetail() == null ? "" : addressBook.getDetail()));
+        this.save(orders);
         //向订单明细表插入数据，多条数据
         //下单完成后，清空购物车数据
     }
