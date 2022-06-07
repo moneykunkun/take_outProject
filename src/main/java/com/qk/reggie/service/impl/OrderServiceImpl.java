7cbe52a152307dff90046ae620d67457b75dd330
35890 <35890226@qq.com>
2022-06-07 18:06:30 +0800

    移动端 完善下单逻辑

--- src/main/java/com/qk/reggie/service/impl/OrderServiceImpl.java
+++ src/main/java/com/qk/reggie/service/impl/OrderServiceImpl.java
@@ -1,15 +1,24 @@
 package com.qk.reggie.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.qk.reggie.common.BaseContext;
+import com.qk.reggie.common.CustomException;
+import com.qk.reggie.entity.AddressBook;
 import com.qk.reggie.entity.Orders;
+import com.qk.reggie.entity.ShoppingCart;
+import com.qk.reggie.entity.User;
 import com.qk.reggie.mapper.OrderMapper;
+import com.qk.reggie.service.AddressBookService;
 import com.qk.reggie.service.OrderService;
 import com.qk.reggie.service.ShoppingCartService;
+import com.qk.reggie.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -17,6 +26,12 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Orders> implement
 
     @Autowired
     private ShoppingCartService shoppingCartService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AddressBookService addressBookService;
     /**
      * 用户下单
      * @param orders
@@ -28,6 +43,24 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Orders> implement
         final Long currentId = BaseContext.getCurrentId();
 
         //查询当前用户的购物车数据
+        LambdaQueryWrapper<ShoppingCart> queryWrapper =new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId,currentId);
+        //查询到购物车数据
+        final List<ShoppingCart> shoppingCarts = shoppingCartService.list(queryWrapper);
+
+        //对购物车数据进行判断
+        if (shoppingCarts ==null){
+            throw  new CustomException("购物车为空，不能下单");
+        }
+        //查询用户信息
+        final User user = userService.getById(currentId);
+
+         Long addressBookId = orders.getAddressBookId();
+         AddressBook addressBook = addressBookService.getById(addressBookId);
+
+         if (addressBook ==null){
+             throw new CustomException("地址信息为空，不能下单");
+         }
         //向订单表中添加数据
         //向订单明细表插入数据，多条数据
         //下单完成后，清空购物车数据
