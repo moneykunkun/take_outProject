98dd7261f253430028d567d3d1af1c92c982ef9c
35890 <35890226@qq.com>
2022-06-07 21:43:32 +0800

    移动端 更新

--- src/main/java/com/qk/reggie/service/OrderDetailService.java
+++ src/main/java/com/qk/reggie/service/OrderDetailService.java
@@ -2,8 +2,11 @@ package com.qk.reggie.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.qk.reggie.entity.OrderDetail;
+import com.qk.reggie.entity.Orders;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface OrderDetailService extends IService<OrderDetail> {
+
+
 }
