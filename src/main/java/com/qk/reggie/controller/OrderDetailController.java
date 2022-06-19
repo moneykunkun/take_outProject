edd25c61a5d3ca3333bc5b747dce02fd37fb2181
35890 <35890226@qq.com>
2022-06-20 00:07:00 +0800

    后台

--- src/main/java/com/qk/reggie/controller/OrderDetailController.java
+++ src/main/java/com/qk/reggie/controller/OrderDetailController.java
@@ -1,12 +1,10 @@
 package com.qk.reggie.controller;
 
-import com.qk.reggie.common.R;
-import com.qk.reggie.entity.Orders;
+
 import com.qk.reggie.service.OrderDetailService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
