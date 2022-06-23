a86a89039368e55d21655a08af478680e8cb3dca
35890 <35890226@qq.com>
2022-06-23 19:35:51 +0800

    更新 后台查询订单明细

--- src/main/java/com/qk/reggie/controller/OrderDetailController.java
+++ src/main/java/com/qk/reggie/controller/OrderDetailController.java
@@ -1,10 +1,14 @@
 package com.qk.reggie.controller;
 
-
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.qk.reggie.common.R;
+import com.qk.reggie.dto.OrdersDto;
 import com.qk.reggie.service.OrderDetailService;
+import com.qk.reggie.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +20,23 @@ public class OrderDetailController {
     @Autowired
     private OrderDetailService orderDetailService;
 
+    @Autowired
+    private OrderService orderService;
+    /**
+     * 后台查询订单明细
+     * @param page
+     * @param pageSize
+     * @param number
+     * @param beginTime
+     * @param endTime
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page<OrdersDto>> empPage(int page, int pageSize, String number,
+                                      String beginTime,
+                                      String endTime) {
+        Page<OrdersDto> empPage = orderService.empPage(page, pageSize, number, beginTime, endTime);
+        return R.success(empPage);
+    }
 
 }
