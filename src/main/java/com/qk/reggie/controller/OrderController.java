2ac11fa31a82bb7ce786298c71c274eb6a0468de
35890 <35890226@qq.com>
2022-06-23 16:43:45 +0800

    更新 后台查询订单明细 派送订单

--- src/main/java/com/qk/reggie/controller/OrderController.java
+++ src/main/java/com/qk/reggie/controller/OrderController.java
@@ -1,15 +1,14 @@
 package com.qk.reggie.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.qk.reggie.common.R;
+import com.qk.reggie.dto.OrdersDto;
 import com.qk.reggie.entity.Orders;
 import com.qk.reggie.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -30,4 +29,33 @@ public class OrderController {
         orderService.submit(orders);
         return  R.success("下单成功");
     }
+
+    /**
+     * 后台查询订单明细
+     * @param page
+     * @param pageSize
+     * @param number
+     * @param beginTime
+     * @param endTime
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page<OrdersDto>> empPage(int page, int pageSize, String number,
+                                      String beginTime,
+                                      String endTime) {
+        Page<OrdersDto> empPage = orderService.empPage(page, pageSize, number, beginTime, endTime);
+        return R.success(empPage);
+    }
+    /**
+     * 派送订单
+     *
+     * @param orders
+     * @return
+     */
+    @PutMapping
+    public R<String> update(@RequestBody Orders orders) {
+        orderService.updateById(orders);
+        return R.success("操作成功");
+    }
+
 }
