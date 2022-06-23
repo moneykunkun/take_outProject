1b2860920877b2b38fefa60d0a269899e67f6c9d
35890 <35890226@qq.com>
2022-06-23 17:17:44 +0800

    更新 订单数据转换对象

--- src/main/java/com/qk/reggie/dto/OrdersDto.java
+++ src/main/java/com/qk/reggie/dto/OrdersDto.java
@@ -2,6 +2,8 @@ package com.qk.reggie.dto;
 
 
 import com.qk.reggie.entity.OrderDetail;
+import com.qk.reggie.entity.Orders;
+import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,19 +11,8 @@ import java.util.List;
 /**
  * 订单数据转换对象
  */
-public class OrdersDto {
+@Data
+public class OrdersDto extends Orders {
     //订单明细
-    public List<OrderDetail>  orderDetails =new ArrayList<>();
-
-    //用户名
-    public String UserName;
-
-    //手机号
-    public String Phone;
-
-    //地址
-    public String Address;
-
-    //
-    public String Consignee;
+    public List<OrderDetail>  OrderDetails =new ArrayList<>();
 }
