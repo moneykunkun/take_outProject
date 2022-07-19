29a27a7c9040ac657f0d113680b3b4a1d92cfccd
35890 <35890226@qq.com>
2022-07-19 23:35:03 +0800

    更新

--- src/main/java/com/qk/reggie/filter/LoginCheckFilter.java
+++ src/main/java/com/qk/reggie/filter/LoginCheckFilter.java
@@ -19,7 +19,7 @@ import java.io.IOException;
 @Slf4j
 @WebFilter(filterName = "loginCheckFilter",urlPatterns = "/*")          //拦截路径
 public class LoginCheckFilter implements Filter {
-    //路径匹配器，支持通配符
+    //路径匹配器，支持通配符，Spring提供的工具包
     public static final AntPathMatcher PATH_MATCHER =new AntPathMatcher();
 
     @Override
