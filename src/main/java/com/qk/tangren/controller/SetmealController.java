811dc5ac6f7474139cc07b2bf798c0fcb691782f
35890 <35890226@qq.com>
2022-08-16 22:25:01 +0800

    项目优化 修改菜品时清理redis中缓存数据

--- src/main/java/com/qk/tangren/controller/SetmealController.java
+++ src/main/java/com/qk/tangren/controller/SetmealController.java
@@ -1,4 +1,4 @@
-package com.qk.reggie.controller;
+package com.qk.tangren.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
