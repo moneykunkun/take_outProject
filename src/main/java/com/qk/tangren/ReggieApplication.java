1f33d2703303bb9176a27f62ff0d38dfa8d45f9a
35890 <35890226@qq.com>
2022-08-18 13:25:01 +0800

    项目优化 新增redis缓存依赖 新增配置文件缓存时间 新增配置类启动缓存注解

--- src/main/java/com/qk/tangren/ReggieApplication.java
+++ src/main/java/com/qk/tangren/ReggieApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Slf4j    //加载日志
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @ServletComponentScan               //扫描过滤器
 @EnableTransactionManagement        //开启事务
+@EnableCaching                      //开启缓存
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class,args);
