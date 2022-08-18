4ad34b9922b13e9a18dc0a9998405e9b7837538c
35890 <35890226@qq.com>
2022-08-18 13:29:42 +0800

    项目优化 新增、删除套餐优化 将套餐缓存数据全部清除

--- src/main/java/com/qk/tangren/controller/SetmealController.java
+++ src/main/java/com/qk/tangren/controller/SetmealController.java
@@ -12,6 +12,7 @@ import com.qk.tangren.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
@@ -40,6 +41,7 @@ public class SetmealController {
      * @return
      */
     @PostMapping
+    @CacheEvict(value = "setmealCache",allEntries = true)               //清除缓存中所有数据
     public R<String> save(@RequestBody SetmealDto setmealDto){
         log.info("套餐信息：{}",setmealDto);
         setmealService.saveWithDish(setmealDto);
@@ -102,6 +104,7 @@ public class SetmealController {
      * @return
      */
     @DeleteMapping
+    @CacheEvict(value = "setmealCache",allEntries = true)               //清除缓存中所有数据
     public R<String> delete(@RequestParam List<Long> ids){
         log.info("ids：{}",ids);
         setmealService.removeWithDish(ids);
